0d6ad57061ab32527b046c408f2d844c2b0e9a63
Kinjal Pathak <kinjal.pathak20061993@gmail.com>
2021-05-07 12:20:22 +0530

    changed database from sqlite to postgre

--- assignment/settings.py
+++ assignment/settings.py
@@ -88,11 +88,33 @@ WSGI_APPLICATION = 'assignment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# DATABASES = {
+#  'default': {
+#      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#      'NAME': 'assignment',
+#      'USER': 'user',
+#      'PASSWORD': 'user',
+#      'HOST': 'localhost',
+#      'PORT': '',  
+#  }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+ 'default': {
+     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+     'NAME': 'assignment',
+     'USER': 'user',
+     'PASSWORD': 'user',
+     'HOST': 'localhost',
+     'PORT': '',  
+ }
 }
 
 
