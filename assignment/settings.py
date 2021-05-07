a9cbb192bb8dc4da7094f11a91b8ffec058f3b02
Kinjal Pathak <kinjal.pathak20061993@gmail.com>
2021-05-07 11:17:08 +0530

    debug true

--- assignment/settings.py
+++ assignment/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jn&y4%q^1*^6si&@9chs#=sim3jvng(e!v97=*8f09q71ryes^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://nurturelabs-django-assignment.herokuapp.com']
 
